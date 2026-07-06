6121006fb704518f2c3187081ebd6d674d3e752c
B SUHAS <bsuhas1460@gmail.com>
2026-07-06 22:38:23 +0530

    Enhance hasDuplicate method to check for duplicates
    Added sorting and checks for empty vector in hasDuplicate function.

--- Data Structures & Algorithms/duplicate-integer/submission-1.cpp
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     bool hasDuplicate(vector<int>& nums) {
+        // Sorting the vector
         sort(nums.begin(),nums.end());
+        // Checking if the vector is empty or not
         if(nums.empty())
         {
             return false;
         }
+        // Comparing the present element to the next element to see if there are any duplicates 
         for(int i=0;i<nums.size()-1;i++)
         {
             
@@ -15,6 +18,7 @@ public:
                 }
 
         }
+        // If duplicates are not founc then returning false
         return false;
     }
 };
