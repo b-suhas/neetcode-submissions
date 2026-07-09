908787a5e774bc439b12f8c70248958a3791ed96
B SUHAS <bsuhas1460@gmail.com>
2026-07-09 11:33:09 +0530

    Enhance isAnagram function with explanatory comments
    Added comments to clarify the logic for checking anagrams.

--- Data Structures & Algorithms/is-anagram/submission-0.cpp
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        // checking if both the string length are equal or not
         if(s.length()!=t.length())
         {
             return false;
         }
+        
+        // sort both for comparing 
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
+        
+        // comparing the string 
         if(s==t)
         {
             return true;
